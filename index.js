903a1c6d89cdd4cca6eff7e5198d7eed60b07523
Andrew <andrewcrink@gmail.com>
2022-10-05 12:54:23 -0400

    cleaned up code and added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Function to simplify adding new Images to document
 function newImage (url, left, bottom) {
     let object = document.createElement('img')
     object.src = url
@@ -7,7 +8,7 @@ function newImage (url, left, bottom) {
     document.body.append(object)
     return object
 }
-
+//Function adds all new Images into screen
 newImage ('assets/green-character.gif', 100, 100)
 newImage ('assets/pine-tree.png', 450, 200)
 newImage ('assets/tree.png', 200, 300)
@@ -15,6 +16,7 @@ newImage ('assets/pillar.png', 350, 100)
 newImage ('assets/crate.png', 150, 200)
 newImage ('assets/well.png', 500, 425)
 
+//adds Items that can be removed with double click
 function newItem(url, left, bottom) {
     let object = newImage(url, left, bottom)
     object.src = url
@@ -28,22 +30,8 @@ function newItem(url, left, bottom) {
         object.remove()
     })
 }
-
+//removable items list
 newItem('assets/sword.png', 500, 405)
 newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
 
-
-//let greenCharacter = document.createElement('img')
-//greenCharacter.src = 'assets/green-character.gif'
-//greenCharacter.style.position = 'fixed'
-//greenCharacter.style.left = '100px'
-//greenCharacter.style.bottom = '100px'
-//document.body.append(greenCharacter)
-
-////let pineTree = document.createElement('img')
-//pineTree.src = 'assets/pine-tree.png'
-//pineTree.style.position = 'fixed'
-//pineTree.style.left = '450px'
-//pineTree.style.bottom = '200px'
-//document.body.append(pineTree)
