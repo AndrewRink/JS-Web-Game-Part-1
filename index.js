0042d7b06af94e45f5084f54c1987f9cd308fed1
Andrew <andrewcrink@gmail.com>
2022-10-06 19:45:28 -0400

    Added background and Inventory feature

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+//creates a tile function so we can split the background between sky and grass
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
 //Function to simplify adding new Images to document
 function newImage (url, left, bottom) {
     let object = document.createElement('img')
@@ -8,30 +18,59 @@ function newImage (url, left, bottom) {
     document.body.append(object)
     return object
 }
-//Function adds all new Images into screen
-newImage ('assets/green-character.gif', 100, 100)
-newImage ('assets/pine-tree.png', 450, 200)
-newImage ('assets/tree.png', 200, 300)
-newImage ('assets/pillar.png', 350, 100)
-newImage ('assets/crate.png', 150, 200)
-newImage ('assets/well.png', 500, 425)
+
 
 //adds Items that can be removed with double click
 function newItem(url, left, bottom) {
-    let object = newImage(url, left, bottom)
-    object.src = url
-    object.position = 'fixed'
-    object.left = left + 'px'
-    object.bottom = bottom +'px'
-    document.body.append(object)
-    
-
-    object.addEventListener('dblclick', function() {
-        object.remove()
+    let item = newImage(url, left, bottom)
+    item.addEventListener('click',function(){
+        item.remove() //adds items to inventory at the bottom
+        let inventoryItem=document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
     })
 }
+
+//sets parameters for tiles for background colors
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+//adds background colors with parameters to have sky and grass
+tile('assets/sky.png', 0, horizon, window.innerWidth/100,heightOfSky/100 )
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+//Function adds all new Images into screen
+newImage ('assets/green-character.gif', 100, 250)
+newImage ('assets/pine-tree.png', 450, 200)
+newImage ('assets/tree.png', 200, 450)
+newImage ('assets/pillar.png', 350, 250)
+newImage ('assets/crate.png', 150, 350)
+newImage ('assets/well.png', 500, 575)
+
 //removable items list
-newItem('assets/sword.png', 500, 405)
-newItem('assets/sheild.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+newItem('assets/sword.png', 500, 555)
+newItem('assets/sheild.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
+
+//Inventory Function
+let inventory;
+
+function newInventory(){
+    inventory = document.createElement('div')
+    inventory.style.position = 'fixed'
+    inventory.style.bottom = '0px';
+    inventory.style.left = '0px'
+    inventory.style.width = '100%'
+    inventory.style.height = '100px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+    inventory.style.border = '2px solid black'
+    inventory.style.backgroundColor = 'brown'
+    document.body.append(inventory)
+}
+
+newInventory()
 
